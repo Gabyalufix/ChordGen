d6107738570c9250bf0cba5a9d002719398f6a4f
gabyalufix <stephen.hartley.jm@gmail.com>
2019-04-23 16:45:52 -0400

    v0.2.12d

--- other.js
+++ other.js
@@ -1,7 +1,7 @@
 NOTE_BASENAMES_SHARP = ["C","C","D","D",
                     "E","F","F","G",
                     "G","A","A","B"]
-NOTE_BASENAMES_FLAT  = ["C","D","D","Eb",
+NOTE_BASENAMES_FLAT  = ["C","D","D","E",
                     "E","F","G","G",
                     "A","A","B","B"]
 
@@ -44,6 +44,73 @@ function addNote(iix){
     //add modSign
 }
 
+var CHORD_PANELSET_EXPAND_BUTTON = document.createElement("button");
+CHORD_PANELSET_EXPAND_BUTTON.textContent = "++";
+//CHORD_PANELSET_EXPAND_BUTTON.classList.add("");
 
+var CHORD_PANELSET_REDUCE_BUTTON = document.createElement("button");
+CHORD_PANELSET_REDUCE_BUTTON.textContent = "--";
+//CHORD_PANELSET_EXPAND_BUTTON.classList.add("");
 
+var CHORD_PANELSET_UP_BUTTON = document.createElement("button");
+CHORD_PANELSET_UP_BUTTON.textContent = "^";
+//CHORD_PANELSET_EXPAND_BUTTON.classList.add("");
+
+var CHORD_PANELSET_DN_BUTTON = document.createElement("button");
+CHORD_PANELSET_DN_BUTTON.textContent = "v";
+//CHORD_PANELSET_EXPAND_BUTTON.classList.add("");
+
+var CHORD_PANELSET_CURRVIS_CT  = 4;
+var CHORD_PANELSET_CURRVIS_IDX = 0;
+var CHORD_PANELSET_PANELS = document.getElementById("CHORD_PANELSET").children
+
+function chordPanelSet_setVisiblePanels(){
+  for(var i=0; i < CHORD_PANELSET_PANELS.length; i++){
+    console.log("panel: "+CHORD_PANELSET_PANELS[i]);
+    if(i < CHORD_PANELSET_CURRVIS_IDX || i >= CHORD_PANELSET_CURRVIS_IDX + CHORD_PANELSET_CURRVIS_CT){
+        CHORD_PANELSET_PANELS[i].style.display = "none";
+    } else {
+        CHORD_PANELSET_PANELS[i].style.display = "grid";
+    }
+  }
+}
+chordPanelSet_setVisiblePanels();
+
+CHORD_PANELSET_EXPAND_BUTTON.onclick = function(){
+    CHORD_PANELSET_CURRVIS_CT = CHORD_PANELSET_CURRVIS_CT + 1;
+    if(CHORD_PANELSET_CURRVIS_IDX + CHORD_PANELSET_CURRVIS_CT >= CHORD_PANELSET_PANELS.length){
+        this.disabled =true;
+    }
+    CHORD_PANELSET_REDUCE_BUTTON.disabled = false;
+    chordPanelSet_setVisiblePanels();
+}
+CHORD_PANELSET_REDUCE_BUTTON.onclick = function(){
+    CHORD_PANELSET_CURRVIS_CT = CHORD_PANELSET_CURRVIS_CT -1;
+    if(CHORD_PANELSET_CURRVIS_CT == 0){
+        this.disabled = true;
+    }
+    CHORD_PANELSET_EXPAND_BUTTON.disabled = false;   
+    chordPanelSet_setVisiblePanels();
+}
+CHORD_PANELSET_UP_BUTTON.onclick = function(){
+    CHORD_PANELSET_CURRVIS_IDX = CHORD_PANELSET_CURRVIS_IDX - 1;
+    if(CHORD_PANELSET_CURRVIS_IDX == 0){
+        this.disabled =true;
+    }
+    CHORD_PANELSET_DN_BUTTON.disabled = false;
+    chordPanelSet_setVisiblePanels();
+}
+CHORD_PANELSET_DN_BUTTON.onclick = function(){
+    CHORD_PANELSET_CURRVIS_IDX = CHORD_PANELSET_CURRVIS_IDX + 1;
+    if(CHORD_PANELSET_CURRVIS_IDX + CHORD_PANELSET_CURRVIS_CT >= CHORD_PANELSET_PANELS.length){
+        this.disabled =true;
+    }
+    CHORD_PANELSET_UP_BUTTON.disabled = false;
+    chordPanelSet_setVisiblePanels();
+}
+
+document.getElementById("CHORD_PANELSET").insertAdjacentElement("afterend",CHORD_PANELSET_EXPAND_BUTTON)
+document.getElementById("CHORD_PANELSET").insertAdjacentElement("afterend",CHORD_PANELSET_REDUCE_BUTTON)
+document.getElementById("CHORD_PANELSET").insertAdjacentElement("afterend",CHORD_PANELSET_UP_BUTTON)
+document.getElementById("CHORD_PANELSET").insertAdjacentElement("afterend",CHORD_PANELSET_DN_BUTTON)
 
