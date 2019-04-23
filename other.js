c721bd8937e8288a9e07c035fcc4b5ac67c44051
gabyalufix <stephen.hartley.jm@gmail.com>
2019-04-23 13:40:38 -0400

    v0.2.10t

--- other.js
+++ other.js
@@ -1,5 +1,48 @@
+NOTE_BASENAMES_SHARP = ["C","C","D","D",
+                    "E","F","F","G",
+                    "G","A","A","B"]
+NOTE_BASENAMES_FLAT  = ["C","D","D","Eb",
+                    "E","F","G","G",
+                    "A","A","B","B"]
+
+NOTE_BASEMOD_SHARP = ["","#","","#",
+                    "","","#","",
+                    "#","","#",""]
+NOTE_BASEMOD_FLAT  = ["","b","","b",
+                    "","","b","",
+                    "b","","b",""]
+
+NOTE_NUMS = {
+    "C":0,
+    "D":1,
+    "E":2,
+    "F":3,
+    "G":4,
+    "A":5,
+    "B":6
+}
 
 
+function getNoteInfo(ixx){
+  var octave = Math.floor(iix / 12);
+  var noteBaseName = NOTE_BASENAMES_SHARP[iix % 12];
+  var noteBaseMod  = NOTE_BASEMOD_SHARP[iix % 12];
+  if(CURRENT_SCALE_SHARPTYPE != "sharp"){
+    noteBaseName = NOTE_BASENAMES_FLAT[iix % 12];
+    noteBaseMod  = NOTE_BASEMOD_FLAT[iix % 12];
+  }
+  var noteNum = NOTE_NUMS[noteBaseName] + (octave * 7)
+  return {baseName: noteBaseName,baseMod: noteBaseMod,octave:octave, noteNum : noteNum};
+}
+
+function addNote(iix){
+    //STAFFLINES;
+    var noteInfo = getNoteInfo(iix);
+    //create note
+    //create modSign?
+    //add note
+    //add modSign
+}
 
 
 
